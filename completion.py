3faa1c5296d278b902e1169b5593c997558f47fb
zhongtao.chen <185763494@qq.com>
2016-06-14 14:03:20 +0800

    first commit

---
+++ completion.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+
+import os
+import sys
+import json
+import logging
+
+
+jsonpath = "/root"
+
+
+logger=logging.getLogger()
+handler=logging.FileHandler("/var/log/messages")
+logger.addHandler(handler)
+logger.setLevel(logging.NOTSET)
+
+def main():
+    logger.info("sys.argv: %s",sys.argv)
+    if len(sys.argv)<2:
+        return
+    filename = sys.argv[1]+".json"
+        
+    jsonfile = os.path.join(jsonpath,filename)
+        
+    fd = open(jsonfile,"r")
+    s1 = fd.read()
+    json1 = json.loads(s1)
+    fd.close()
+    
+    subcmd = json1
+    for i in range(2, len(sys.argv)-1):
+        if isinstance(subcmd,dict):
+            if subcmd.has_key(sys.argv[i]):
+                if sys.argv[i].startswith("--"):
+                    subcmd.pop(sys.argv[i])
+                else:
+                    subcmd = subcmd[sys.argv[i]]
+            else:
+                continue
+        elif isinstance(subcmd,list):
+            if sys.argv[i] in subcmd:
+                subcmd.remove(sys.argv[i])
+            if sys.argv[i]+"=" in subcmd:
+                subcmd.remove(sys.argv[i]+"=")
+            else:
+                continue
+        else:
+            return
+    if isinstance(subcmd,dict):
+        options = subcmd.keys()
+    elif isinstance(subcmd,list):
+        options = subcmd
+    else:
+        return
+    for key in options:
+        if key.startswith(sys.argv[-1]) or sys.argv[-1] == "*":
+            print key
+
+
+if __name__ == "__main__":
+    sys.exit(main())
+
+
---
+++ completion.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+function __cmd_completion() {  
+    case $COMP_CWORD in
+    0)
+        ;;
+    1|*)  
+        if [ -z "${COMP_WORDS[COMP_CWORD]}" ]
+        then
+                COMP_WORDS[$COMP_CWORD]="\*"
+        fi
+        COMPREPLY=( $(eval /etc/mycompletion/completion.py ${COMP_WORDS[*]}))
+        ;;
+    esac
+}
